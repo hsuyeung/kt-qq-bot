07b00b8c54c0c5a17c6be8b812e4cc260ceff22e
xuyang9978 <xuyang9978@gmail.com>
2021-03-15 16:46:31 +0800

    创建项目

---
+++ src/main/kotlin/run/xuyang/ktqqbot/KtQqBotApplication.kt
@@ -0,0 +1,11 @@
+package run.xuyang.ktqqbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtQqBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtQqBotApplication>(*args)
+}
---
+++ src/test/kotlin/run/xuyang/ktqqbot/KtQqBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package run.xuyang.ktqqbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtQqBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
