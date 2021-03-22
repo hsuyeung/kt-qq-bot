541452369104a0379b0f686508d1efc90fe0568a
xuyang9978 <xuyang9978@gmail.com>
2021-03-22 23:13:02 +0800

    feat: 使用 kotlin 改写 Poem 实体类

---
+++ src/main/kotlin/run/xuyang/ktqqbot/entity/Poem.kt
@@ -0,0 +1,14 @@
+package run.xuyang.ktqqbot.entity
+
+/**
+ * 今日诗词 API 返回结果实体类
+ *
+ * @author XuYang
+ * @date 2021/3/21 23:37
+ */
+class Poem {
+    var content: String? = null
+    var origin: String? = null
+    var author: String?= null
+    var category: String?= null
+}
